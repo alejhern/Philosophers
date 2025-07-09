51bde9ce87c3f5d4b124cd39889eb233a520e86f
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2025-07-09 18:40:10 +0200

    fichero de utilidades filosoficas

---
+++ utils.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 14:14:49 by alejhern          #+#    #+#             */
+/*   Updated: 2025/07/09 14:14:55 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher.h"
+
+long	ft_atol(const char *str)
+{
+	long	result;
+	long	sign;
+
+	result = 0;
+	sign = 1;
+	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str)
+	{
+		if (*str >= '0' || *str <= '9')
+			result = result * 10 + *str - '0';
+		else
+			return (0);
+		str++;
+	}
+	return (sign * result);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	t_size;
+	void	*arr;
+	size_t	index;
+
+	if (nmemb > 0 && size > 0 && nmemb > SIZE_MAX / size)
+		return (NULL);
+	t_size = nmemb * size;
+	arr = malloc(t_size);
+	if (!arr)
+		return (NULL);
+	index = 0;
+	while (index < t_size)
+	{
+		((char *)arr)[index] = '\0';
+		index++;
+	}
+	return (arr);
+}
+
+long	timestamp_ms(void)
+{
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
